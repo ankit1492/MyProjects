4925580b41ec8e50401c7ff749450f5819ce7575
Ankit Singh <ankit.krsingh@optimusinfo.com>
2017-02-19 21:12:58 +0530

    Added the Basic XAML Controls app

---
+++ XAMLBasicAppDemo/XAMLBasicAppDemo/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using XAMLBasicAppDemo.ViewModels;
+using XAMLBasicAppDemo.Views;
+
+namespace XAMLBasicAppDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var studentDetails = new StudentDetails();
+            var studentsViewModel = new StudentDetailsViewModel();
+            studentDetails.DataContext = studentsViewModel;
+            studentDetails.Show();
+        }
+    }
+}
---
+++ XAMLBasicAppDemo/XAMLBasicAppDemo/ViewModels/StudentDetailsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XAMLBasicAppDemo.Models;
+
+namespace XAMLBasicAppDemo.ViewModels
+{
+    public class StudentDetailsViewModel
+    {
+        private List<Student> _students;
+
+        public StudentDetailsViewModel()
+        {
+            _students = GetAllStudents();
+        }
+
+        private List<Student> GetAllStudents()
+        {
+            var students = new List<Student>();
+            students.Add(new Student { Id = 1, Name = "Ram", Department = "IT" });
+            students.Add(new Student { Id = 2, Name = "Shyam", Department = "CS" });
+            students.Add(new Student { Id = 3, Name = "Seeta", Department = "ME" });
+            students.Add(new Student { Id = 4, Name = "Geeta", Department = "EC" });
+            return students;
+        }
+
+        public List<Student> Students
+        {
+            get { return _students; }
+        }
+    }
+}
